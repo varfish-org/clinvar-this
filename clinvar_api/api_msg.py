90afd105fc0d29c3962529157f38a20e48090a56
Manuel Holtgrewe <manuel.holtgrewe@bihealth.de>
2022-11-11 16:04:27 +0100

    feat: implement models for submission message (#8) (#15)

--- clinvar_api/api_msg.py
+++ clinvar_api/api_msg.py
@@ -179,3 +179,192 @@ class SummaryResponse:
     totalDeleted: typing.Optional[int] = None
     totalDeleteErrors: typing.Optional[int] = None
     totalDeleteSuccess: typing.Optional[int] = None
+
+
+@attrs.define
+class SubmissionClinvarDeletionAccessionSet:
+    accession: str
+    reason: typing.Optional[str] = None
+
+
+@attrs.define
+class SubmissionClinvarDeletion:
+    accessionSet: typing.List[SubmissionClinvarDeletionAccessionSet]
+
+
+@attrs.define
+class SubmissionChromosomeCoordinates:
+    accession: typing.Optional[str] = None
+    assembly: typing.Optional[str] = None
+    chromosome: typing.Optional[str] = None
+    innerStart: typing.Optional[int] = None
+    innerStop: typing.Optional[int] = None
+    outerStart: typing.Optional[int] = None
+    outerStop: typing.Optional[int] = None
+    referenceAllele: typing.Optional[str] = None
+    start: typing.Optional[int] = None
+    stop: typing.Optional[int] = None
+    variantLength: typing.Optional[int] = None
+
+
+@attrs.define
+class SubmissionVariantGene:
+    id: typing.Optional[int] = None
+    symbol: typing.Optional[str] = None
+
+
+@attrs.define
+class SubmissionVariant:
+    chromosomeCoordinates: typing.Optional[SubmissionChromosomeCoordinates] = None
+    copyNumber: typing.Optional[str] = None
+    gene: typing.Optional[typing.List[SubmissionVariantGene]] = None
+    hgvs: typing.Optional[str] = None
+    referenceCopyNumber: typing.Optional[int] = None
+    variantType: typing.Optional[str] = None
+
+
+@attrs.define
+class SubmissionVariantSet:
+    variant: typing.List[SubmissionVariant]
+
+
+@attrs.define
+class SubmissionPhaseUnknownSet:
+    hgvs: str
+    variants: typing.List[SubmissionVariant]
+
+
+@attrs.define
+class SubmissionClinicalFeature:
+    clinicalFeaturesAffectedStatus: str
+    db: typing.Optional[str] = None
+    id: typing.Optional[str] = None
+    name: typing.Optional[str] = None
+
+
+@attrs.define
+class SubmissionObservedIn:
+    affectedStatus: str
+    alleleOrigin: str
+    collectionMethod: str
+    clinicalFeatures: typing.Optional[typing.List[SubmissionClinicalFeature]] = None
+    clinicalFeaturesComment: typing.Optional[str] = None
+    numberOfIndividuals: typing.Optional[int] = None
+    structVarMethodType: typing.Optional[str] = None
+
+
+@attrs.define
+class SubmissionHaplotypeSet:
+    hgvs: str
+    variants: typing.List[SubmissionVariant]
+    starAlleleName: typing.Optional[str] = None
+
+
+@attrs.define
+class SubmissionDistinctChromosomesSet:
+    hgvs: str
+    #: Hast at least two elements
+    variants: typing.List[SubmissionVariant]
+
+
+@attrs.define
+class SubmissionHaplotypeSets:
+    haplotypeSet: typing.Optional[SubmissionHaplotypeSet] = None
+    haplotypeSingleVariantSet: typing.Optional[SubmissionHaplotypeSet] = None
+
+
+@attrs.define
+class SubmissionDisplotypeSet:
+    haplotypeSets: typing.List[SubmissionHaplotypeSets]
+    hgvs: str
+    starAlleleName: typing.Optional[str] = None
+
+
+@attrs.define
+class SubmissionCitation:
+    db: typing.Optional[str] = None
+    id: typing.Optional[str] = None
+    url: typing.Optional[str] = None
+
+
+@attrs.define
+class SubmissionAssertionCriteria:
+    citation: SubmissionCitation
+    method: str
+
+
+@attrs.define
+class SubmissionCondition:
+    db: typing.Optional[str] = None
+    id: typing.Optional[str] = None
+    name: typing.Optional[str] = None
+
+
+@attrs.define
+class SubmissionDrugResponse:
+    db: typing.Optional[str] = None
+    drugName: typing.Optional[str] = None
+    id: typing.Optional[str] = None
+    condition: typing.Optional[typing.List[SubmissionCondition]] = None
+
+
+@attrs.define
+class SubmissionConditionSet:
+    condition: typing.Optional[typing.List[SubmissionCondition]] = None
+    drugResponse: typing.Optional[typing.List[SubmissionDrugResponse]] = None
+
+
+@attrs.define
+class SubmissionCompoundHeterozygoteSetVariantSet:
+    variantSet: typing.Optional[SubmissionVariantSet] = None
+
+
+@attrs.define
+class SubmissionCompoundHeterozygoteSet:
+    hvs: str
+    # Must have two entries
+    variantSets: typing.List[SubmissionCompoundHeterozygoteSetVariantSet]
+
+
+@attrs.define
+class SubmissionClinicalSignificance:
+    clinicalSignificanceDescription: str
+    citation: typing.Optional[typing.List[SubmissionCitation]] = None
+    comment: typing.Optional[str] = None
+    customAssertionScore: typing.Optional[float] = None
+    dateLastEvaluated: typing.Optional[str] = None
+    explanationOfDrugResponse: typing.Optional[str] = None
+    explanationOfOtherClinicalSignificance: typing.Optional[str] = None
+    modeOfInheritance: typing.Optional[str] = None
+
+
+@attrs.define
+class SubmissionClinvarSubmission:
+    clinicalSignificance: SubmissionClinicalSignificance
+    conditionSet: SubmissionConditionSet
+    observedIn: typing.List[SubmissionObservedIn]
+    recordStatus: str
+    releaseStatus: str
+    assertionCriteria: typing.Optional[SubmissionAssertionCriteria] = None
+    clinvarAccession: typing.Optional[str] = None
+    compoundHeterozygoteSet: typing.Optional[SubmissionCompoundHeterozygoteSet] = None
+    diplotypeSet: typing.Optional[SubmissionDisplotypeSet] = None
+    distinctChromosomesSet: typing.Optional[SubmissionDistinctChromosomesSet] = None
+    #: Has at least two elements in `variants`
+    haplotypeSet: typing.Optional[SubmissionHaplotypeSet] = None
+    #: Has exactly one elements in `variants`
+    haplotypeSingleVariantSet: typing.Optional[SubmissionHaplotypeSet] = None
+    localID: typing.Optional[str] = None
+    localKey: typing.Optional[str] = None
+    phaseUnknownSet: typing.Optional[SubmissionPhaseUnknownSet] = None
+    variantSet: typing.Optional[SubmissionVariantSet] = None
+
+
+@attrs.define
+class SubmissionContainer:
+    """Representation of the container for a submission."""
+
+    behalfOrgID: typing.Optional[int] = None
+    clinvarDeletion: typing.Optional[SubmissionClinvarDeletion] = None
+    clinvarSubmission: typing.Optional[typing.List[SubmissionClinvarSubmission]] = None
+    submissionName: typing.Optional[str] = None
