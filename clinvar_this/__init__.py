4e5ad7eb1d477f65bc801c1f9916381513a07358
Manuel Holtgrewe <manuel.holtgrewe@bih-charite.de>
2023-09-04 08:16:32 +0200

    feat: removing versioneer in favour of version from release-please

--- clinvar_this/__init__.py
+++ clinvar_this/__init__.py
@@ -2,8 +2,7 @@
 
 __author__ = """Manuel Holtgrewe"""
 __email__ = "manuel.holtgrewe@bih-charite.de"
-__version__ = "0.1.0"
 
-from . import _version
+from clinvar_this import _version
 
-__version__ = _version.get_versions()["version"]
+__version__ = _version.__version__
