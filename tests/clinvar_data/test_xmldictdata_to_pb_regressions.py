7eec141acf866472046de148f2d74b6024504fcc
Manuel Holtgrewe <manuel.holtgrewe@bih-charite.de>
2024-07-01 06:10:47 +0200

    fix: namespaced string issue from 2207-07-01

---
+++ tests/clinvar_data/test_xmldictdata_to_pb_regressions.py
@@ -0,0 +1,22 @@
+"""Regression tests for issues occuring in weekly builds of clinvar-data-jsonl"""
+
+import json
+
+from google.protobuf.json_format import MessageToDict, Parse
+import pytest
+
+from clinvar_data.conversion.dict_to_pb import ConvertClassifiedRecord
+
+@pytest.mark.parametrize(
+    "path_json_input",
+    [
+        "tests/clinvar_data/data/regressions/2024-07-01/allele-id-1706807.json",
+        "tests/clinvar_data/data/regressions/2024-07-01/allelle-id-1704898.json",
+    ]
+)
+def test_convert_classified_record_xmldict_data_to_pb(path_json_input: str, snapshot):
+    with open(path_json_input, "rt") as input:
+        xmldict_value = json.load(input)
+    result = ConvertClassifiedRecord.xmldict_data_to_pb(xmldict_value)
+    result_json = MessageToDict(result)
+    snapshot.assert_match(json.dumps(result_json, indent=2), "result")
